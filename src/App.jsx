b1284ec7137cf6f398fdfa3ad4b038724f1518e5
avil00liva <agustinoli54@gmail.com>
2021-07-05 22:00:29 -0300

    hasrouter prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom"
+import {HashRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom"
 import Home from './pages';
 import AddProducts from './components/addProducts/AddProducts';
 import { ProductsContextProvider } from './global/ProductsContext';
